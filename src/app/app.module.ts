ff21d71642732aa1124112d06e24a0e51611c626
Vasyl Bomba <vasyl.bomba@embiq.com>
2018-10-31 13:38:37 +0100

    up ang, users-details added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RaportsComponent } from './raports/raports.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DataHttpService } from './services/data-http.service';
 import { RegistrationComponent } from './registration/registration.component';
+import { UsersDetailsComponent } from './users-details/users-details.component';
 
 
 const appRoutes: Routes = [
@@ -20,6 +21,7 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent },
   { path: 'books', component: BooksComponent },
   { path: 'raports', component: RaportsComponent },
+  { path: 'users-details', component: UsersDetailsComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: '**', redirectTo: 'main' }
 ];
@@ -31,6 +33,7 @@ const appRoutes: Routes = [
     UsersComponent,
     RaportsComponent,
     RegistrationComponent,
+    UsersDetailsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/users-details/users-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-users-details',
+  templateUrl: './users-details.component.html',
+  styleUrls: ['./users-details.component.css']
+})
+export class UsersDetailsComponent implements OnInit {
+
+  numbers: number[] = [];
+  books: number[] = [];
+  ngOnInit() {}
+  constructor() {
+  for (let i = 0; i < 100; i++) {
+      this.numbers.push( i );
+  }
+}
+drop ( event: CdkDragDrop<number[]> ) {
+  if ( event.previousContainer !== event.container) {
+    transferArrayItem( event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex)
+  } else {
+moveItemInArray ( this.numbers, event.previousIndex, event.currentIndex );
+}
+}
+}
